018b3ef670b7c33781045ed794573bfcb93c0f09
Despo <tornike.despotashvili@gmail.com>
2024-05-04 20:34:55 +0400

    setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-
+import { Route, Routes } from "react-router-dom"
+import Home from "./Home"
 
 function App() {
 
   return (
-    <>
-      sad
-    </>
+    <div className="app">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+    </div>
   )
 }
 
